108c0155d9e943825ee6ae851649292efbd19e51
Lukas Svoboda <lukas.svoboda@gmail.com>
2015-09-16 11:54:17 +0200

    test: fix specs on static_machines

--- spec/unit/lab_manager/provider/static_machine_spec.rb
+++ spec/unit/lab_manager/provider/static_machine_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
 describe Provider::StaticMachine do
+  let(:config) do
+    {
+      'static1' =>  { ip: '10.5.0.1' },
+      'static2' =>  { ip: '10.5.0.2' }
+    }
+  end
+  before(:each) do
+    allow(Provider::StaticMachineConfig).to receive(:machines) { config }
+  end
+
   describe "::filter_machines_to_be_scheduled" do
-    xit "returns scheduleble machines" do
+    it "returns schedulable machines" do
       compute1 = create(:compute, provider_name: 'static_machine', name: 'static1')
       compute2 = create(:compute, provider_name: 'static_machine', name: 'static1')
       res = Provider::StaticMachine.filter_machines_to_be_scheduled
@@ -11,9 +21,10 @@ describe Provider::StaticMachine do
   end
 
   describe "#create_vm" do
-    #TODO: mock StaticMachineConfig
     it "raises NotFound when StaticMachine does not exist" do
-      compute = build(:compute, provider_name: :static_machine, name: 'does-not-exist')
+      compute = build(:compute,
+                      provider_name: :static_machine,
+                      name: 'does-not-exist')
       static_machine = Provider::StaticMachine.new(compute)
       expect do
         static_machine.create_vm({})
