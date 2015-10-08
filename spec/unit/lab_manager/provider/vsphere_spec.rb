7bc13437ca4c48fab2e9d4c34f165b3cf49e87e5
Ivo Reznicek <ireznice@googlemail.com>
2015-10-08 14:30:15 +0200

    unittests: vsphere: reboot, shutdown

--- spec/unit/lab_manager/provider/vsphere_spec.rb
+++ spec/unit/lab_manager/provider/vsphere_spec.rb
@@ -166,7 +166,7 @@ describe Provider::VSphere do
     end
   end
 
-  describe '#power_on' do
+  describe '#poweron_vm' do
     it 'fails when provider data are not present' do
       provider.compute.provider_data = nil
       expect { provider.poweron_vm }.to raise_error(ArgumentError)
@@ -210,7 +210,7 @@ describe Provider::VSphere do
         server
       end
 
-      allow(vsphere_mock).to receive(:servers).and_return(servers)
+      expect(vsphere_mock).to receive(:servers).and_return(servers)
       provider.compute.provider_data = { 'id' => 'fo' }
       provider.terminate_vm
     end
@@ -220,10 +220,193 @@ describe Provider::VSphere do
       server = double('server to be terminated', destroy: { 'task_state' => 'failed' })
       allow(servers).to receive(:get) { server }
 
-      allow(vsphere_mock).to receive(:servers).and_return(servers)
+      expect(vsphere_mock).to receive(:servers).at_least(:once).and_return(servers)
       provider.compute.provider_data = { 'id' => 'fo' }
       expect(server).to receive(:destroy).at_least(:twice)
       expect { provider.terminate_vm }.to raise_error(Provider::VSphere::TerminateVmError)
+    end
+  end
+
+  describe '#shutdown_vm' do
+    let(:servers) { double('servers') }
+    let(:server) { double('server to be terminated') }
+
+    it 'fails when provider data are not present' do
+      provider.compute.provider_data = nil
+      expect { provider.shutdown_vm }.to raise_error(ArgumentError)
+    end
+
+    it 'fails when the machine does not exist' do
+      expect(servers).to receive(:get).at_least(:once) { nil }
+
+      provider.compute.provider_data = { 'id' => 'fo' }
+      expect(vsphere_mock).to receive(:servers).at_least(:once).and_return(servers)
+      expect { provider.shutdown_vm }.to raise_error(Provider::VSphere::VmNotExistsError)
+    end
+
+    it 'does nothing when the machine is already powered off' do
+      expect(server).to receive(:power_state).at_least(:once) { 'poweredOff' }
+      expect(servers).to receive(:get).at_least(:once) { server }
+      expect(provider).to receive(:set_provider_data).once
+      expect(vsphere_mock).to receive(:servers).at_least(:once).and_return(servers)
+      expect(server).to receive(:stop).at_most(0).times
+      provider.compute.provider_data = { 'id' => 'fo' }
+      provider.shutdown_vm
+    end
+
+    context 'managed mode' do
+      it 'calls server.stop with force when soft stop failed' do
+        second_time = false
+        expect(server).to receive(:power_state).at_least(:twice) do
+          second_time ? 'poweredOff' : 'poweredOn'
+        end
+        expect(server).to receive(:stop).twice do |param|
+          expect(param[:force]).to eq second_time
+          second_time = true
+          fail 'soft stop failed' if second_time
+        end
+        expect(servers).to receive(:get).at_least(:once) { server }
+
+        expect(vsphere_mock).to receive(:servers).at_least(:once).and_return(servers)
+        expect(vsphere_mock).to receive(:get_virtual_machine).at_least(:once) do
+          { 'power_state' => 'poweredOff' }
+        end
+        provider.compute.provider_data = { 'id' => 'fo' }
+        provider.shutdown_vm
+      end
+    end
+
+    context 'hard mode' do
+      it 'calls server.stop with force' do
+        expect(server).to receive(:power_state).at_least(:twice) { 'poweredOn' }
+        expect(server).to receive(:stop).at_least(:once) do |param|
+          expect(param[:force]).to eq true
+          fail 'Expected function called'
+        end
+
+        expect(servers).to receive(:get).at_least(:once) { server }
+        expect(vsphere_mock).to receive(:servers).at_least(:once).and_return(servers)
+        provider.compute.provider_data = { 'id' => 'fo' }
+        expect { provider.shutdown_vm(mode: 'hard') }.to raise_error('Expected function called')
+      end
+    end
+
+    context 'soft mode' do
+      it 'calls server.stop without force' do
+        expect(server).to receive(:power_state).at_least(:twice) { 'poweredOn' }
+        expect(server).to receive(:stop).at_least(:once) do |param|
+          expect(param[:force]).to eq false
+          fail 'Expected function called'
+        end
+
+        expect(servers).to receive(:get).at_least(:once) { server }
+        expect(vsphere_mock).to receive(:servers).at_least(:once).and_return(servers)
+        provider.compute.provider_data = { 'id' => 'fo' }
+        expect { provider.shutdown_vm(mode: 'soft') }.to raise_error('Expected function called')
+      end
+    end
+
+    context 'unknown mode' do
+      it 'throws an exception' do
+        expect(server).to receive(:power_state).at_least(:twice) { 'poweredOn' }
+        expect(server).to receive(:stop).at_most(0).times do
+          fail 'Expected function called'
+        end
+
+        expect(servers).to receive(:get).at_least(:once) { server }
+        expect(vsphere_mock).to receive(:servers).at_least(:once).and_return(servers)
+        provider.compute.provider_data = { 'id' => 'fo' }
+        expect do
+          provider.shutdown_vm(mode: 'something unexpected')
+        end.to raise_error(Provider::VSphere::ShutdownVmError)
+      end
+    end
+
+    it 'refreshes provider data' do
+      expect(server).to receive(:power_state).at_least(:once) { 'poweredOff' }
+      expect(servers).to receive(:get).at_least(:once) { server }
+      expect(provider).to receive(:set_provider_data).once
+      expect(vsphere_mock).to receive(:servers).at_least(:once).and_return(servers)
+      provider.compute.provider_data = { 'id' => 'fo' }
+      provider.shutdown_vm
+    end
+  end
+
+  describe '#reboot_vm' do
+    let(:servers) { double('servers') }
+    let(:server) { double('server to be rebooted') }
+
+    it 'fails when provider data are not present' do
+      provider.compute.provider_data = nil
+      expect { provider.reboot_vm }.to raise_error(ArgumentError)
+    end
+
+    it 'fails when the machine does not exist' do
+      expect(servers).to receive(:get).at_least(:once) { nil }
+
+      provider.compute.provider_data = { 'id' => 'fo' }
+      expect(vsphere_mock).to receive(:servers).at_least(:once).and_return(servers)
+      expect { provider.shutdown_vm }.to raise_error(Provider::VSphere::VmNotExistsError)
+    end
+
+    context 'managed mode' do
+      it 'calls server.reboot with force when soft reboot failed' do
+        second_time = false
+        expect(server).to receive(:reboot).at_least(:twice) do |param|
+          expect(param[:force]).to eq second_time
+          second_time = !second_time
+          fail 'soft reboot failed' if second_time
+        end
+
+        expect(servers).to receive(:get).at_least(:once) { server }
+
+        expect(vsphere_mock).to receive(:servers).at_least(:once).and_return(servers)
+        provider.compute.provider_data = { 'id' => 'fo' }
+        provider.reboot_vm
+      end
+    end
+
+    context 'hard mode' do
+      it 'calls server.reboot with force' do
+        expect(server).to receive(:reboot).at_least(:once) do |param|
+          expect(param[:force]).to eq true
+          fail 'Expected function called'
+        end
+
+        expect(servers).to receive(:get).at_least(:once) { server }
+        expect(vsphere_mock).to receive(:servers).at_least(:once).and_return(servers)
+        provider.compute.provider_data = { 'id' => 'fo' }
+        expect { provider.reboot_vm(mode: 'hard') }.to raise_error('Expected function called')
+      end
+    end
+
+    context 'soft mode' do
+      it 'calls server.reboot without force' do
+        expect(server).to receive(:reboot).at_least(:once) do |param|
+          expect(param[:force]).to eq false
+          fail 'Expected function called'
+        end
+
+        expect(servers).to receive(:get).at_least(:once) { server }
+        expect(vsphere_mock).to receive(:servers).at_least(:once).and_return(servers)
+        provider.compute.provider_data = { 'id' => 'fo' }
+        expect { provider.reboot_vm(mode: 'soft') }.to raise_error('Expected function called')
+      end
+    end
+
+    context 'unknown mode' do
+      it 'throws an exception' do
+        expect(server).to receive(:reboot).at_most(0).times do
+          fail 'Expected function called'
+        end
+
+        expect(servers).to receive(:get).at_least(:once) { server }
+        expect(vsphere_mock).to receive(:servers).at_least(:once).and_return(servers)
+        provider.compute.provider_data = { 'id' => 'fo' }
+        expect do
+          provider.reboot_vm(mode: 'something unexpected')
+        end.to raise_error(Provider::VSphere::RebootVmError)
+      end
     end
   end
 end
