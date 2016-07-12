0929b4c0590ff6cf3963695940a0a1c010e72dfa
Ivo Reznicek <ireznice@googlemail.com>
2016-07-12 11:47:54 +0200

    feature: medusa production

--- lib/lab_manager/provider/static_machine_config.rb
+++ lib/lab_manager/provider/static_machine_config.rb
@@ -3,7 +3,7 @@ require 'settingslogic'
 module Provider
   # Configuration of the StaticMachine provider
   class StaticMachineConfig < Settingslogic
-    source "#{LabManager.root}/config/static_machine.yml"
+    source "#{LabManager.root}/config/provider_static_machine.yml"
     namespace LabManager.env
   end
 end
